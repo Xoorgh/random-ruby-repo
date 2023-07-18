4d11cef4afdb1b5033319836f015fa37f5713088
Xoorgh <viktorfasth87@gmail.com>
2023-07-19 01:45:05 +0200

    Remove methods

--- change_splitter.rb
+++ change_splitter.rb
@@ -6,19 +6,16 @@ AVAILABLE_DENOMINATIONS = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
 puts "Sum to return: "
 sum = gets.chomp.to_i
 
-denomination_calc(sum)
+@distribution = []
 
-def denomination_calc(sum)
-	@distribution = []
-	AVAILABLE_DENOMINATIONS.reverse_each do |denomination|
-		count = []
+AVAILABLE_DENOMINATIONS.reverse_each do |denomination|
+	count = []
 
-		count << denomination
-		count << (sum / denomination).to_i
-		@distribution << count
-		sum -= count.inject(:*)
-	end
-	@distribution.each do |pair|
-		puts "#{pair[0]}: #{pair[1]}" unless pair[1] == 0
-	end
+	count << denomination
+	count << (sum / denomination).to_i
+	@distribution << count
+	sum -= count.inject(:*)
+end
+@distribution.each do |pair|
+	puts "#{pair[0]}: #{pair[1]}" unless pair[1] == 0
 end
